aeff5297d51e4d949f70bea8720ae353366e4040
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-12-05 10:40:57 +0900

    update devDependencies
    Highlight: spaces -> tabs https://github.com/shinnn/eslint-config/commit/e92384d999f10fd0e477e6ada6b979c48977a2f7

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ const enumerateFiles = require('enumerate-files');
 const isFileUtf8 = require('is-file-utf8');
 
 module.exports = async function listUtf8Files(...args) {
-  const paths = await enumerateFiles(...args);
-  const results = new Set();
-  const promises = new Set();
+	const paths = await enumerateFiles(...args);
+	const results = new Set();
+	const promises = new Set();
 
-  for (const path of paths) {
-    promises.add(isFileUtf8(path));
-  }
+	for (const path of paths) {
+		promises.add(isFileUtf8(path));
+	}
 
-  const pathIterator = paths.values();
+	const pathIterator = paths.values();
 
-  for (const isUtf8 of await Promise.all(promises)) {
-    const {value} = pathIterator.next();
+	for (const isUtf8 of await Promise.all(promises)) {
+		const {value} = pathIterator.next();
 
-    if (isUtf8) {
-      results.add(value);
-    }
-  }
+		if (isUtf8) {
+			results.add(value);
+		}
+	}
 
-  return results;
+	return results;
 };
--- test.js
+++ test.js
@@ -12,63 +12,63 @@ const promisifiedMkdir = promisify(mkdir);
 const promisifiedWriteFile = promisify(writeFile);
 
 test('listUtf8File()', async t => {
-  t.plan(7);
+	t.plan(7);
 
-  const tmp = join(__dirname, 'tmp');
+	const tmp = join(__dirname, 'tmp');
 
-  await rmfr(tmp);
-  await promisifiedMkdir(tmp);
-  await Promise.all([
-    promisifiedWriteFile(join(tmp, 'empty-file'), ''),
-    promisifiedWriteFile(join(tmp, '__non-utf8___'), Buffer.from([1, 2, 3])),
-    promisifiedWriteFile(join(tmp, '2'), 'a'),
-    promisifiedWriteFile(join(tmp, '10'), 'b'),
-    promisifiedMkdir(join(tmp, '__dir__'))
-  ]);
+	await rmfr(tmp);
+	await promisifiedMkdir(tmp);
+	await Promise.all([
+		promisifiedWriteFile(join(tmp, 'empty-file'), ''),
+		promisifiedWriteFile(join(tmp, '__non-utf8___'), Buffer.from([1, 2, 3])),
+		promisifiedWriteFile(join(tmp, '2'), 'a'),
+		promisifiedWriteFile(join(tmp, '10'), 'b'),
+		promisifiedMkdir(join(tmp, '__dir__'))
+	]);
 
-  listUtf8File(tmp, {numeric: true}).then(files => {
-    t.ok(files instanceof Set, 'should be fulfilled with a Set instance.');
+	listUtf8File(tmp, {numeric: true}).then(files => {
+		t.ok(files instanceof Set, 'should be fulfilled with a Set instance.');
 
-    t.deepEqual([...files], [
-      '2',
-      '10',
-      'empty-file'
-    ].map(path => join(tmp, path)), 'should list directories in a given directory.');
-  }).catch(t.fail);
+		t.deepEqual([...files], [
+			'2',
+			'10',
+			'empty-file'
+		].map(path => join(tmp, path)), 'should list directories in a given directory.');
+	}).catch(t.fail);
 
-  listUtf8File('not-found').catch(err => {
-    t.equal(err.code, 'ENOENT', 'should fail when it cannot find the directory.');
-  });
+	listUtf8File('not-found').catch(err => {
+		t.equal(err.code, 'ENOENT', 'should fail when it cannot find the directory.');
+	});
 
-  listUtf8File([0, 1]).catch(err => {
-    t.equal(
-      err.toString(),
-      'TypeError: Expected a directory path (string), but got [ 0, 1 ] (array).',
-      'should fail when it takes a non-string path.'
-    );
-  });
+	listUtf8File([0, 1]).catch(err => {
+		t.equal(
+			err.toString(),
+			'TypeError: Expected a directory path (string), but got [ 0, 1 ] (array).',
+			'should fail when it takes a non-string path.'
+		);
+	});
 
-  listUtf8File('Hi', 0).catch(err => {
-    t.equal(
-      err.toString(),
-      'TypeError: The second argument of readdir-sorted must be a plain object, but got 0 (number).',
-      'should fail when it takes a non-object option.'
-    );
-  });
+	listUtf8File('Hi', 0).catch(err => {
+		t.equal(
+			err.toString(),
+			'TypeError: The second argument of readdir-sorted must be a plain object, but got 0 (number).',
+			'should fail when it takes a non-object option.'
+		);
+	});
 
-  listUtf8File().catch(err => {
-    t.equal(
-      err.toString(),
-      'TypeError: Expected 1 or 2 arguments (path: String[, options: Object]), but got no arguments.',
-      'should fail when it takes no arguments.'
-    );
-  });
+	listUtf8File().catch(err => {
+		t.equal(
+			err.toString(),
+			'TypeError: Expected 1 or 2 arguments (path: String[, options: Object]), but got no arguments.',
+			'should fail when it takes no arguments.'
+		);
+	});
 
-  listUtf8File(1, 2, 3).catch(err => {
-    t.equal(
-      err.toString(),
-      'TypeError: Expected 1 or 2 arguments (path: String[, options: Object]), but got 3 arguments.',
-      'should fail when it takes too many arguments.'
-    );
-  });
+	listUtf8File(1, 2, 3).catch(err => {
+		t.equal(
+			err.toString(),
+			'TypeError: Expected 1 or 2 arguments (path: String[, options: Object]), but got 3 arguments.',
+			'should fail when it takes too many arguments.'
+		);
+	});
 });
